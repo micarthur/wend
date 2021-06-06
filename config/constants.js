90c2f67b599c592bd066d3554df547230e284257
micarthur <micarthur@utexas.edu>
2021-06-06 15:47:18 -0500

    Added Logo

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,6 @@
-const facebookAppId = "410790490033541";
-const facebookDisplayName = "DAP App";
+const facebookAppId = "3541877239190759";
+const facebookDisplayName = "Wend App";
 const googleIOSClientId =
-	"810038729061-cs0la8rrug6nei2epnkacdtdn79diitg.apps.googleusercontent.com";
+  "69883081036-rqc3pblt5nnk85eils362a0itubljgfl.apps.googleusercontent.com";
 const googleAndroidClientId =
-	"810038729061-6t78hl9qh9fusaff1itoil9t9ne8g0s5.apps.googleusercontent.com";
+  "810038729061-6t78hl9qh9fusaff1itoil9t9ne8g0s5.apps.googleusercontent.com";
--- screens/splash/splash.js
+++ screens/splash/splash.js
@@ -4,12 +4,12 @@ import { Button } from "react-native-paper";
 import * as Facebook from "expo-facebook";
 import styles from "./styles";
 import {
-	facebookAppId,
-	googleIOSClientId,
-	googleAndroidClientId,
+  facebookAppId,
+  googleIOSClientId,
+  googleAndroidClientId,
 } from "../../config/constants";
 
-const image = require("./fatherson.jpeg");
+const image = require("./wend.jpeg");
 
 // const initSocialLogin = async () => {
 // 	try {
@@ -69,113 +69,108 @@ const image = require("./fatherson.jpeg");
 // };
 
 async function toggleFacebookAuthAsync() {
-	await Facebook.initializeAsync(facebookAppId);
-	const auth = await Facebook.getAuthenticationCredentialAsync();
-	let body;
+  await Facebook.initializeAsync(facebookAppId);
+  const auth = await Facebook.getAuthenticationCredentialAsync();
+  let body;
 
-	if (!auth) {
-		body = await logInFacebook();
-	} else {
-		console.log(auth);
-		const resolvedToken = auth.token;
-		const response = await fetch(
-			`https://graph.facebook.com/me?fields=email,name&access_token=${resolvedToken}`
-		);
-		body = await response.json();
-		// // alert(`Hi ${body.name}!`);
-		// console.log(body);
-	}
-	return { auth: true, data: body };
+  if (!auth) {
+    body = await logInFacebook();
+  } else {
+    console.log(auth);
+    const resolvedToken = auth.token;
+    const response = await fetch(
+      `https://graph.facebook.com/me?fields=email,name&access_token=${resolvedToken}`
+    );
+    body = await response.json();
+    // // alert(`Hi ${body.name}!`);
+    // console.log(body);
+  }
+  return { auth: true, data: body };
 }
 
 async function logInFacebook() {
-	try {
-		await Facebook.initializeAsync({
-			appId: facebookAppId,
-		});
-		const {
-			type,
-			token,
-			expirationDate,
-			permissions,
-			declinedPermissions,
-		} = await Facebook.logInWithReadPermissionsAsync({
-			permissions: ["email", "public_profile"],
-		});
-		if (type === "success") {
-			// Get the user's name using Facebook's Graph API
-			const response = await fetch(
-				`https://graph.facebook.com/me?fields=email,name&access_token=${token}`
-			);
-			const data = await response.json();
-			return data;
-		} else {
-			// type === 'cancel'
-		}
-	} catch ({ message }) {
-		alert(`Facebook Login Error: ${message}`);
-	}
+  try {
+    await Facebook.initializeAsync({
+      appId: facebookAppId,
+    });
+    const { type, token, expirationDate, permissions, declinedPermissions } =
+      await Facebook.logInWithReadPermissionsAsync({
+        permissions: ["email", "public_profile"],
+      });
+    if (type === "success") {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(
+        `https://graph.facebook.com/me?fields=email,name&access_token=${token}`
+      );
+      const data = await response.json();
+      return data;
+    } else {
+      // type === 'cancel'
+    }
+  } catch ({ message }) {
+    alert(`Facebook Login Error: ${message}`);
+  }
 }
 
 export default class Splash extends Component {
-	static navigationOptions = {
-		headerShown: false,
-	};
-	render() {
-		return (
-			<ImageBackground source={image} style={styles.image}>
-				<SafeAreaView style={{ flex: 1 }}>
-					<View style={styles.containerMain}>
-						<View style={styles.bottomViewGoogle}>
-							<Button
-								icon="google"
-								mode="contained"
-								style={styles.buttonGoogle}
-								onPress={() =>
-									this.props.navigation.navigate("Home", { name: "user" })
-								}
-							>
-								<Text style={styles.textStyleSocial}>Sign In With Google</Text>
-							</Button>
-						</View>
-						<View style={styles.bottomViewFacebook}>
-							<Button
-								icon="facebook"
-								mode="contained"
-								style={styles.buttonFacebook}
-								onPress={async () => {
-									const response = await toggleFacebookAuthAsync();
-									if (response.auth) {
-										console.log(response);
-										this.props.navigation.navigate("Home", {
-											name: response.data.name,
-											// name: "user",
-										});
-									}
-								}}
-							>
-								<Text style={styles.textStyleSocial}>Sign In With FB</Text>
-							</Button>
-						</View>
-						<View style={styles.bottomView}>
-							<Button
-								mode="contained"
-								style={styles.button}
-								onPress={() => this.props.navigation.navigate("Register")}
-							>
-								<Text style={styles.textStyle}>Sign Up</Text>
-							</Button>
-							<Button
-								mode="contained"
-								style={styles.button}
-								onPress={() => this.props.navigation.navigate("Login")}
-							>
-								<Text style={styles.textStyle}>Sign In</Text>
-							</Button>
-						</View>
-					</View>
-				</SafeAreaView>
-			</ImageBackground>
-		);
-	}
+  static navigationOptions = {
+    headerShown: false,
+  };
+  render() {
+    return (
+      <ImageBackground source={image} style={styles.image}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={styles.containerMain}>
+            <View style={styles.bottomViewGoogle}>
+              <Button
+                icon="google"
+                mode="contained"
+                style={styles.buttonGoogle}
+                onPress={() =>
+                  this.props.navigation.navigate("Home", { name: "user" })
+                }
+              >
+                <Text style={styles.textStyleSocial}>Sign In With Google</Text>
+              </Button>
+            </View>
+            <View style={styles.bottomViewFacebook}>
+              <Button
+                icon="facebook"
+                mode="contained"
+                style={styles.buttonFacebook}
+                onPress={async () => {
+                  const response = await toggleFacebookAuthAsync();
+                  if (response.auth) {
+                    console.log(response);
+                    this.props.navigation.navigate("Home", {
+                      name: response.data.name,
+                      // name: "user",
+                    });
+                  }
+                }}
+              >
+                <Text style={styles.textStyleSocial}>Sign In With FB</Text>
+              </Button>
+            </View>
+            <View style={styles.bottomView}>
+              <Button
+                mode="contained"
+                style={styles.button}
+                onPress={() => this.props.navigation.navigate("Register")}
+              >
+                <Text style={styles.textStyle}>Sign Up</Text>
+              </Button>
+              <Button
+                mode="contained"
+                style={styles.button}
+                onPress={() => this.props.navigation.navigate("Login")}
+              >
+                <Text style={styles.textStyle}>Sign In</Text>
+              </Button>
+            </View>
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
+    );
+  }
 }
